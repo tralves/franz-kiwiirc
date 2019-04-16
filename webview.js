818d5f200d9d5582486dd1441777eaba95e46488
Tiago Alves <tralves@gmail.com>
2019-04-16 15:08:01 +0100

    Fix unread count.

--- webview.js
+++ webview.js
@@ -1,31 +1,32 @@
-'use strict'
+'use strict';
 
-const path = require('path')
+const path = require('path');
 
 module.exports = Franz => {
-  const getMessages = function getMessages() {
-    const unreadChannelsCount = kiwi.state.networks.reduce((count, network) => {
-      return (count += network.buffers.filter($buffer => {
-        return $buffer.flags.unread !== 0
-      }).length)
-    }, 0)
+    const getMessages = function getMessages() {
+        const unreadChannelsCount = kiwi.state.networks.reduce((count, network) => {
+            return (count += network.buffers.filter(buffer => {
+                return !buffer.name.startsWith('*') && buffer.flags.unread !== 0;
+            }).length);
+        }, 0);
 
-    const mentionedChannelsCount = kiwi.state.networks.reduce(
-      (count, network) => {
-        return (count += network.buffers.filter(buffer => {
-          return buffer.flags.unread !== 0 && buffer.flags.highlight
-        }).length)
-      },
-      0
-    )
+        const mentionedChannelsCount = kiwi.state.networks.reduce((count, network) => {
+            return (count += network.buffers.filter(buffer => {
+                return (
+                    !buffer.name.startsWith('*') &&
+                    buffer.flags.unread !== 0 &&
+                    buffer.flags.highlight
+                );
+            }).length);
+        }, 0);
 
-    // set Franz badges
-    Franz.setBadge(mentionedChannelsCount, unreadChannelsCount)
-  }
+        // set Franz badges
+        Franz.setBadge(mentionedChannelsCount, unreadChannelsCount);
+    };
 
-  // inject franz.css stylesheet
-  Franz.injectCSS(path.join(__dirname, 'service.css'))
+    // inject franz.css stylesheet
+    Franz.injectCSS(path.join(__dirname, 'service.css'));
 
-  // check for new messages every second and update Franz badge
-  Franz.loop(getMessages)
-}
+    // check for new messages every second and update Franz badge
+    Franz.loop(getMessages);
+};
